39f4fb5944d20fcc1b096951369a5fd38d3f1497
prince-sunsara <princesunasara977@gmail.com>
2023-08-29 17:03:50 +0530

    added validation to user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,40 @@
 const express = require("express")
 const router = express.Router();
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-// create user using: POST "/api/auth/"
-router.get('/', (req, res) => {
-    console.log(req.body)
-    res.send(req.body)
-}).post('/', (req, res) => {
-    const user = User(req.body)
-    user.save();
-    res.send(req.body)
+// create user using: POST "/api/auth/createUser": no login require
+router.post('/createuser', [
+    body('name', 'Name must have atleast 3 char').isLength({ min: 3 }),
+    body('email', 'Enter valid email').isEmail(),
+    body('password', 'Password must have atleast 5 char').isLength({ min: 5 }),
+], async (req, res) => {
+    // if there are errors return 400 and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // check whether user with this email exist already
+    try {
+        let user = await User.findOne({ email: req.body.email })
+        
+        if (!user) {
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            });
+            return res.status(200).json(user)
+
+        } else {
+            return res.status(400).json({ error: "Sorry! user with this email is exist" })
+        }
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Oops! some error occured..)")
+    }
 })
 
 module.exports = router;
