cbbbec840a62238fd5b0b15eb5edceb0a68aa54a
prince-sunsara <princesunasara977@gmail.com>
2023-08-30 20:07:17 +0530

    added updated note method

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -45,4 +45,30 @@ router.post('/addnote', fetchuser, [
     }
 })
 
+// ROUTE 3 : Update an existing note using: POST "/api/notes/updatenote": login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+    const { title, description, tag } = req.body;
+
+    // creating new note object
+    const newNote = {};
+    if (title) { newNote.title = title }
+    if (description) { newNote.description = description }
+    if (tag) { newNote.tag = tag }
+
+    // find the note to be updated 
+    let note = await Notes.findById(req.params.id);
+    if (!note) { return res.status(404).send("Not found!") }
+
+    // check user
+    // console.log(note.user)
+    if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not allowed!")
+    }
+
+    // update note with new entries
+    note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json(note);
+})
+
 module.exports = router;
