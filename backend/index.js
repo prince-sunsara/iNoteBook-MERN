c9beb00d4efa9ebfd6b286c446c75260215355dd
prince-sunsara <princesunasara977@gmail.com>
2023-09-03 03:26:22 +0530

    connected dynamic frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const connectToMongo = require('./db');
 const express = require("express");
 const app = express();
+var cors = require('cors')
 const port = 5000;
 
 // mongo connection
 connectToMongo();
 
+ 
+app.use(cors()) // for cors error when adding note in backend using frontend
 app.use(express.json());
 
 // Available routes
